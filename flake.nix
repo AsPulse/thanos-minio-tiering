31b22ffc53c57aaa5bf1da64567b263879073613
AsPulse / あすぱる <contact@aspulse.dev>
2024-06-29 17:15:43 +0900

    setup rust projects

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake for developing thanos-minio-tiering";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            libiconv
+          ];
+          buildInputs = with pkgs; [
+            rustup
+          ];
+          shellHook = ''
+            exec $SHELL
+          '';
+        };
+      }
+    );
+}
